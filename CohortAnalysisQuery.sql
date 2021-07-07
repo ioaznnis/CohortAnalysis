b1603c8223db8a8993d4b48d4c89cb09d4011e07
Ivan Anzin <1431061+ioaznnis@users.noreply.github.com>
2021-07-07 16:52:46 +0300

    Update CohortAnalysisQuery.sql

--- CohortAnalysisQuery.sql
+++ CohortAnalysisQuery.sql
@@ -1,21 +1,20 @@
 select 
    Cohort
- , sum(m0) m0
- , sum(m1) m1 
- , sum(m2) m2  
- , sum(m3) m3  
- , sum(m4) m4  
- , sum(m5) m5  
- , sum(m6) m6  
- , sum(m7) m7  
- , sum(m8) m8  
- , sum(m9) m9  
- , sum(m10) m10 
- , sum(m11) m11 
+ , sum(m0) Month0
+ , sum(m1) Month1 
+ , sum(m2) Month2  
+ , sum(m3) Month3  
+ , sum(m4) Month4  
+ , sum(m5) Month5  
+ , sum(m6) Month6  
+ , sum(m7) Month7  
+ , sum(m8) Month8  
+ , sum(m9) Month9  
+ , sum(m10) Month10 
+ , sum(m11) Month11 
 from 
 (SELECT
     Clients.CreatedAt Cohort
---   , Clients.PhoneNumber AS PhoneNumber
   ,(SELECT IIF(COUNT(O.DateAdded) = 0 , 0, 1) FROM Orders O WHERE O.DateAdded = DATEADD(MONTH, 0, Clients.CreatedAt) AND O.PhoneNumber = Clients.PhoneNumber) AS  m0
   ,(SELECT IIF(COUNT(O.DateAdded) = 0 , 0, 1) FROM Orders O WHERE O.DateAdded = DATEADD(MONTH, 1, Clients.CreatedAt) AND O.PhoneNumber = Clients.PhoneNumber) AS  m1
   ,(SELECT IIF(COUNT(O.DateAdded) = 0 , 0, 1) FROM Orders O WHERE O.DateAdded = DATEADD(MONTH, 2, Clients.CreatedAt) AND O.PhoneNumber = Clients.PhoneNumber) AS  m2
@@ -28,14 +27,14 @@ from
   ,(SELECT IIF(COUNT(O.DateAdded) = 0 , 0, 1) FROM Orders O WHERE O.DateAdded = DATEADD(MONTH, 9, Clients.CreatedAt) AND O.PhoneNumber = Clients.PhoneNumber) AS  m9
   ,(SELECT IIF(COUNT(O.DateAdded) = 0 , 0, 1) FROM Orders O WHERE O.DateAdded = DATEADD(MONTH, 10, Clients.CreatedAt) AND O.PhoneNumber = Clients.PhoneNumber) AS m10
   ,(SELECT IIF(COUNT(O.DateAdded) = 0 , 0, 1) FROM Orders O WHERE O.DateAdded = DATEADD(MONTH, 11, Clients.CreatedAt) AND O.PhoneNumber = Clients.PhoneNumber) AS m11
-FROM (select
+FROM (select                                                                                                                                                       
           PhoneNumber, 
           min(DateAdded) CreatedAt
     from Orders
     group by PhoneNumber
     ) Clients
  ) UserActivityFlow
--- check continuity order by month (optional)
+-- check continuity order by month
 where m0 = 1 or m1 = 0 and m2 = 0 and m3 = 0 and m4 = 0 and m5 = 0 and m6 = 0 and m7 = 0 and m8 = 0 and m9 = 0 and m10 = 0 and m11 = 0
    or m0 = 1 or m1 = 1 and m2 = 0 and m3 = 0 and m4 = 0 and m5 = 0 and m6 = 0 and m7 = 0 and m8 = 0 and m9 = 0 and m10 = 0 and m11 = 0
    or m0 = 1 or m1 = 1 and m2 = 1 and m3 = 0 and m4 = 0 and m5 = 0 and m6 = 0 and m7 = 0 and m8 = 0 and m9 = 0 and m10 = 0 and m11 = 0
